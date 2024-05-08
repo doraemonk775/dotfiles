49c9bf2415559441314bb45acfc7439153e5cad3
doraemonk775 <riku.ogura.775@gmail.com>
2024-05-08 21:27:12 +0900

    added

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,9 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.expandtab = true
+vim.opt.ignorecase = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.showtabline = 4
 vim.opt.tabstop = 4
