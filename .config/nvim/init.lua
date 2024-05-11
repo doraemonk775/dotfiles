55ce0b35f260b0d6dd2e8be963373a37119d7649
doraemonk775 <riku.ogura.775@gmail.com>
2024-05-12 08:54:55 +0900

    added

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,10 @@ vim.opt.shiftwidth = 4
 vim.opt.showtabline = 4
 vim.opt.tabstop = 4
 
+vim.keymap.set("c", "<C-a>", "<Home>", { noremap = true, silent = false })
+vim.keymap.set("c", "<C-f>", "<Right>", { noremap = true, silent = false })
+vim.keymap.set("c", "<C-b>", "<Left>", { noremap = true, silent = false })
+vim.keymap.set("c", "<C-e>", "<End>", { noremap = true, silent = false })
 vim.keymap.set("n", "[t", ":tabprevious<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "]t", ":tabnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
