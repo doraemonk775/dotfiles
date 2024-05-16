d0bcfbbf40f9658c6122e11c5ceb7add1357945a
doraemonk775 <riku.ogura.775@gmail.com>
2024-05-16 20:06:00 +0900

    コマンドライン履歴を保存しない

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,8 @@ vim.opt.shiftwidth = 4
 vim.opt.showtabline = 4
 vim.opt.tabstop = 4
 
+vim.cmd("set viminfo+=:0")
+
 vim.keymap.set("c", "<C-a>", "<Home>", { noremap = true, silent = false })
 vim.keymap.set("c", "<C-f>", "<Right>", { noremap = true, silent = false })
 vim.keymap.set("c", "<C-b>", "<Left>", { noremap = true, silent = false })
