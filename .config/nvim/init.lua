1a3ac1eba9d617767622777b64de76561de1fd6c
doraemonk775 <riku.ogura.775@gmail.com>
2024-05-20 19:38:12 +0900

    modified

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,29 @@
-vim.opt.encoding = 'utf-8'
-vim.opt.number = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.cursorline = true
+vim.o.scrolloff = 8
+vim.o.autoread = true
+vim.o.swapfile = false
+vim.o.clipboard = 'unnamedplus'
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-vim.opt.cursorline = true
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.swapfile = false
-
-vim.opt.relativenumber = true
-vim.opt.showtabline = 4
+vim.o.relativenumber = true
+vim.o.showtabline = 4
 
 vim.cmd('set viminfo+=:0')
 
-vim.keymap.set('c', '<C-a>', '<Home>', { noremap = true, silent = false })
-vim.keymap.set('c', '<C-f>', '<Right>', { noremap = true, silent = false })
-vim.keymap.set('c', '<C-b>', '<Left>', { noremap = true, silent = false })
-vim.keymap.set('c', '<C-e>', '<End>', { noremap = true, silent = false })
-vim.keymap.set('n', '[t', ':tabprevious<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', ']t', ':tabnext<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '[T', ':tabfirst<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', ']T', ':tablast<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('c', '<C-a>', '<Home>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('c', '<C-f>', '<Right>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('c', '<C-b>', '<Left>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('c', '<C-e>', '<End>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '[t', ':tabprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']t', ':tabnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '[T', ':tabfirst<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ']T', ':tablast<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
