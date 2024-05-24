a6a9960fa886a59f5e9e62708e979105f1ec61d8
doraemonk775 <riku.ogura.775@gmail.com>
2024-05-24 20:36:09 +0900

    fzf使ってみよう

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,3 +28,6 @@ require('lazy').setup({
     { 'junegunn/fzf', run = function() vim.fn['fzf#install']() end },
     'junegunn/fzf.vim',
 })
+
+vim.g.mapleader = ' '
+vim.api.nvim_set_keymap('n', '<leader>f', ':Files<CR>', { noremap = true, silent = true })
