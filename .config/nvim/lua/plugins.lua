f701c8ef0716b2927d30804f899d0c484746e1b4
doraemonk775 <riku.ogura.775@gmail.com>
2024-06-01 20:40:16 +0900

    fzf.vimをアンインストール

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,7 +2,5 @@ require('lazy').setup({
     { 'cocopon/iceberg.vim', config = function() require('plugin_configs.iceberg') end },
     { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
     { 'akinsho/bufferline.nvim', version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' } },
-    { 'junegunn/fzf', run = function() vim.fn['fzf#install']() end },
-    'junegunn/fzf.vim',
     { 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
 })
