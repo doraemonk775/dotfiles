502da77db9c7059cbcb85f0e816b921b20903c54
doraemonk775 <riku.ogura.775@gmail.com>
2024-06-02 16:54:32 +0900

    colorschemeをnightfox.nvimに乗換え

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,5 +1,5 @@
 require('lazy').setup({
-    { 'cocopon/iceberg.vim', config = function() require('plugin_configs.iceberg') end },
+    { 'EdenEast/nightfox.nvim', config = function() require('plugin_configs.nightfox') end },
     { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
     { 'akinsho/bufferline.nvim', version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' } },
     { 'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = { 'nvim-lua/plenary.nvim' } },
