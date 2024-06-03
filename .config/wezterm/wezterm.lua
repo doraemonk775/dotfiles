41ff7d63b4ea8968c0bc464e45c75113edaf4a96
doraemonk775 <riku.ogura.775@gmail.com>
2024-06-04 07:25:18 +0900

    added

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,8 @@ config.font_size = 12
 config.initial_cols = 160
 config.initial_rows = 48
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 config.window_background_opacity = 0.8
 
 return config
