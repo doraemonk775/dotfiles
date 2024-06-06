0acc2f6e7b68fe3404362ffe75e78d11ee0f9fb7
doraemonk775 <riku.ogura.775@gmail.com>
2024-06-07 06:07:48 +0900

    tmuxを使ってみよう！

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,11 +4,11 @@ local config = wezterm.config_builder()
 config.color_scheme = 'carbonfox'
 config.font = wezterm.font('UDEV Gothic NFLG', { weight = 'Bold' })
 config.font_size = 12
+config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 160
 config.initial_rows = 48
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
 config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
 config.window_background_opacity = 0.8
 
 return config
